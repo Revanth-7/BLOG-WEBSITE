b40fb9a6304a33395f729b20f2ac89fc3565b58d
Revanth <revcr07@gmail.com>
2021-06-06 19:56:21 +0530

    make it better

--- app.js
+++ app.js
@@ -73,8 +73,8 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port");
 });
